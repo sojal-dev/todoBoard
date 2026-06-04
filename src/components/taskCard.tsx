d038369f6b8057a647844ef97fcf05be17f00c86
Sojal <sojalsaini816@gmail.com>
2026-06-04 12:51:05 +0530

    Add delete task functionality

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -1,5 +1,8 @@
-import { Chip } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 import type { Task } from "../types/task";
+import { useBoardStore } from "../store/boardStore";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
 
 interface TaskCardProps {
     task: Task;
@@ -20,10 +23,27 @@ const TaskCard = ({ task }: TaskCardProps) => {
         }
     };
 
+    const deleteTask = useBoardStore(
+        (state) => state.deleteTask
+    );
+
     return(
-        <div className="task-card d-flex gap-2 align-items-center justify-content-between">
-            <h4 className="mb-0">{task.title}</h4>
-            <Chip label={task.priority.toUpperCase()} color={getPriorityColor()} size="small" />
+        <div className="task-card">
+            <div className="d-flex gap-2 align-items-center justify-content-between">
+                <h4 className="mb-0">{task.title}</h4>
+                <div className="d-flex gap-3 align-items-center">
+                    <div className="delete-btn">
+                            <IconButton
+                                size="small"
+                                color="error"
+                                onClick={() => deleteTask(task.id)}
+                            >
+                                <DeleteIcon fontSize="medium" />
+                            </IconButton>
+                        </div>
+                    <Chip label={task.priority.toUpperCase()} color={getPriorityColor()} size="small" />
+                </div>
+            </div>
         </div>
     )
 };
--- src/store/boardStore.ts
+++ src/store/boardStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { Task } from "../types/task";
 import { todoTasks } from "../utils/todoTasks";
 
@@ -8,26 +9,33 @@ interface BoardStore {
   deleteTask: (id: string) => void;
 }
 
-export const useBoardStore = create<BoardStore>((set) => ({
-  tasks: todoTasks,
+export const useBoardStore = create<BoardStore>()(
+  persist(
+    (set) => ({
+      tasks: todoTasks,
 
-  addTask: (column) =>
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: crypto.randomUUID(),
-          title: "New Task",
-          description: "",
-          priority: "medium",
-          column,
-          order: state.tasks.length,
-        },
-      ],
-    })),
+      addTask: (column) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: crypto.randomUUID(),
+              title: "New Task",
+              description: "",
+              priority: "medium",
+              column,
+              order: state.tasks.length,
+            },
+          ],
+        })),
 
-  deleteTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id),
-    })),
-}));
+      deleteTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+    }),
+    {
+      name: "todoBoard-storage",
+    }
+  )
+);
